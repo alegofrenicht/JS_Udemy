6fcda32d211b5348913212f7fcf8f9c2f7f7fee2
Alex <alexander-replyansky@yandex.ru>
2024-06-16 00:21:03 +0300

    adding form validation

--- TMap/script.js
+++ TMap/script.js
@@ -100,7 +100,30 @@ class App {
     };
 
     _newWorkout(e){
+        const validInputs = (...inputs) => inputs.every(inp => Number.isFinite(inp));
+        const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+        
         e.preventDefault();
+
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value; 
+        console.log(type);
+
+
+        if (type === "running"){
+            const cadence = +inputCadence.value;
+            if (
+                !validInputs(distance, duration, cadence) || !allPositive(distance, duration, cadence))
+                return alert("Inputs have to be positive numbers!");
+            }
+
+        if (type === "cycling"){
+            const elevation = +inputElevation.value;
+            if (!validInputs(distance, duration, elevation) || !allPositive(distance, duration)) 
+                return alert("Inputs have to be positive numbers!");
+            }
+
         
         inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
         const {lat, lng} = this.#mapEvent.latlng;
