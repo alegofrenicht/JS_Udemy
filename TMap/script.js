4cafe3d16648793d50a96ffe5c2f76bbc3aa756b
Alex Replyansky <alexr@rapid-domain.com>
2024-06-18 17:13:31 +0300

    editing workout creating code

--- TMap/script.js
+++ TMap/script.js
@@ -1,5 +1,5 @@
 'use strict';
-let counter = 1;
+
 
 
 const form = document.querySelector('.form');
@@ -55,8 +55,10 @@ class Cycling extends Workout {
 // console.log(cycl);
 
 class App {
+    #counter = 1;
     #map;
     #mapEvent;
+    #workouts = [];
 
     constructor(){
         this._getPosition();
@@ -102,13 +104,13 @@ class App {
     _newWorkout(e){
         const validInputs = (...inputs) => inputs.every(inp => Number.isFinite(inp));
         const allPositive = (...inputs) => inputs.every(inp => inp > 0);
-        
         e.preventDefault();
 
         const type = inputType.value;
         const distance = +inputDistance.value;
         const duration = +inputDuration.value; 
-        console.log(type);
+        const {lat, lng} = this.#mapEvent.latlng;
+        let workout;
 
 
         if (type === "running"){
@@ -116,32 +118,41 @@ class App {
             if (
                 !validInputs(distance, duration, cadence) || !allPositive(distance, duration, cadence))
                 return alert("Inputs have to be positive numbers!");
+
+            workout = new Running([lat, lng], distance, duration, cadence);
             }
 
         if (type === "cycling"){
             const elevation = +inputElevation.value;
             if (!validInputs(distance, duration, elevation) || !allPositive(distance, duration)) 
                 return alert("Inputs have to be positive numbers!");
+
+            workout = new Cycling([lat, lng], distance, duration, elevation);
             }
 
+        this.#workouts.push(workout);
+        console.log(workout);
+        console.log(this.#workouts);
         
-        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
-        const {lat, lng} = this.#mapEvent.latlng;
         L.marker([lat, lng]).addTo(this.#map)
-        .bindPopup(L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: "running-popup",
+        .bindPopup(
+            L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: `${type}-popup`,
     
         }
         )
         )
-        .setPopupContent(`Workout`)
+        .setPopupContent(`Workout ${this.#counter}`)
         .openPopup();
+
+        form.classList.add("hidden");
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
     
-        // counter ++;
+        this.#counter ++;
     }
 };
 
