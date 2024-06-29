65da78cfbb33abdf189f95e265067609f8edbb4b
Alex <alexander-replyansky@yandex.ru>
2024-06-29 20:10:19 +0300

    starting asynchronous js chapter + completing app working with api with info about countries using callbacks

--- test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// const startTimer = function (){
-  // setInterval(() => new Intl.DateTimeFormat('en').format('5 hours'))
-// };
-
-console.log(new Intl.DateTimeFormat('en', {dateStyle: 'time'}).format('05:00:00'))
